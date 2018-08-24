22f8823a04f8972dad751ee2b2218991b4a2c2e7
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-08-24 20:09:30 +0200

    min, maal en gedeeld door doen nu ook wat ze van plan waren

--- app/src/main/java/com/verpoorten/wim/factcalculator/MainActivity.java
+++ app/src/main/java/com/verpoorten/wim/factcalculator/MainActivity.java
@@ -46,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
 
         text_calculation = (EditText) findViewById(R.id.text_calculation);
 
+        // when the 0 button gets clicked
         input_0 = (Button) findViewById(R.id.button_0);
         input_0.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 1 button gets clicked
         input_1 = (Button) findViewById(R.id.button_1);
         input_1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -66,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 2 button gets clicked
         input_2 = (Button) findViewById(R.id.button_2);
         input_2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 3 button gets clicked
         input_3 = (Button) findViewById(R.id.button_3);
         input_3.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -86,6 +90,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 4 button gets clicked
         input_4 = (Button) findViewById(R.id.button_4);
         input_4.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,6 +101,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 5 button gets clicked
         input_5 = (Button) findViewById(R.id.button_5);
         input_5.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,6 +112,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 6 button gets clicked
         input_6 = (Button) findViewById(R.id.button_6);
         input_6.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -116,6 +123,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 7 button gets clicked
         input_7 = (Button) findViewById(R.id.button_7);
         input_7.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -126,6 +134,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 8 button gets clicked
         input_8 = (Button) findViewById(R.id.button_8);
         input_8.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -136,6 +145,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the 9 button gets clicked
         input_9 = (Button) findViewById(R.id.button_9);
         input_9.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -146,6 +156,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the + button gets clicked
         input_add = (Button) findViewById(R.id.button_add);
         input_add.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -162,7 +173,7 @@ public class MainActivity extends AppCompatActivity {
 
                 } else if ( init_value.contains("-") ) {
 
-                    String getallen[] = init_value.split("\\-");
+                    String getallen[] = init_value.split("-");
                     int getal1 =  Integer.parseInt( getallen[0] );
                     int getal2 =  Integer.parseInt( getallen[1] );
                     int uitkomst = getal1 - getal2;
@@ -171,20 +182,20 @@ public class MainActivity extends AppCompatActivity {
 
                 } else if ( init_value.contains("x") ) {
 
-                    String getallen[] = init_value.split("\\x");
+                    String getallen[] = init_value.split("x");
                     int getal1 =  Integer.parseInt( getallen[0] );
                     int getal2 =  Integer.parseInt( getallen[1] );
                     int uitkomst = getal1 * getal2;
-                    String new_value = Integer.toString(uitkomst) + "x";
+                    String new_value = Integer.toString(uitkomst) + "+";
                     text_calculation.setText(new_value);
 
                 } else if ( init_value.contains(":") ) {
 
-                    String getallen[] = init_value.split("\\:");
+                    String getallen[] = init_value.split(":");
                     int getal1 =  Integer.parseInt( getallen[0] );
                     int getal2 =  Integer.parseInt( getallen[1] );
                     int uitkomst = getal1 / getal2;
-                    String new_value = Integer.toString(uitkomst) + ":";
+                    String new_value = Integer.toString(uitkomst) + "+";
                     text_calculation.setText(new_value);
 
                 } else {
@@ -194,5 +205,151 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // when the - button gets clicked
+        input_subtract = (Button) findViewById(R.id.button_subtract);
+        input_subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String init_value = text_calculation.getText().toString();
+                if (init_value.contains("+") ) {
+
+                    String getallen[] = init_value.split("\\+");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 + getal2;
+                    String new_value = Integer.toString(uitkomst) + "-";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains("-") ) {
+
+                    String getallen[] = init_value.split("-");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 - getal2;
+                    String new_value = Integer.toString(uitkomst) + "-";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains("x") ) {
+
+                    String getallen[] = init_value.split("x");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 * getal2;
+                    String new_value = Integer.toString(uitkomst) + "-";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains(":") ) {
+
+                    String getallen[] = init_value.split(":");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 / getal2;
+                    String new_value = Integer.toString(uitkomst) + "-";
+                    text_calculation.setText(new_value);
+
+                } else {
+                    final String new_value = init_value + "-";
+                    text_calculation.setText(new_value);
+                }
+            }
+        });
+
+        // when the x button gets clicked
+        input_multiply = (Button) findViewById(R.id.button_multiply);
+        input_multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String init_value = text_calculation.getText().toString();
+                if (init_value.contains("+") ) {
+
+                    String getallen[] = init_value.split("\\+");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 + getal2;
+                    String new_value = Integer.toString(uitkomst) + "x";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains("-") ) {
+
+                    String getallen[] = init_value.split("-");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 - getal2;
+                    String new_value = Integer.toString(uitkomst) + "x";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains("x") ) {
+
+                    String getallen[] = init_value.split("x");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 * getal2;
+                    String new_value = Integer.toString(uitkomst) + "x";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains(":") ) {
+
+                    String getallen[] = init_value.split(":");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 / getal2;
+                    String new_value = Integer.toString(uitkomst) + "x";
+                    text_calculation.setText(new_value);
+
+                } else {
+                    final String new_value = init_value + "x";
+                    text_calculation.setText(new_value);
+                }
+            }
+        });
+
+        // when the : button gets clicked
+        input_divide = (Button) findViewById(R.id.button_divide);
+        input_divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String init_value = text_calculation.getText().toString();
+                if (init_value.contains("+") ) {
+
+                    String getallen[] = init_value.split("\\+");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 + getal2;
+                    String new_value = Integer.toString(uitkomst) + ":";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains("-") ) {
+
+                    String getallen[] = init_value.split("-");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 - getal2;
+                    String new_value = Integer.toString(uitkomst) + ":";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains("x") ) {
+
+                    String getallen[] = init_value.split("x");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 * getal2;
+                    String new_value = Integer.toString(uitkomst) + ":";
+                    text_calculation.setText(new_value);
+
+                } else if ( init_value.contains(":") ) {
+
+                    String getallen[] = init_value.split(":");
+                    int getal1 =  Integer.parseInt( getallen[0] );
+                    int getal2 =  Integer.parseInt( getallen[1] );
+                    int uitkomst = getal1 / getal2;
+                    String new_value = Integer.toString(uitkomst) + ":";
+                    text_calculation.setText(new_value);
+
+                } else {
+                    final String new_value = init_value + ":";
+                    text_calculation.setText(new_value);
+                }
+            }
+        });
     }
 }
