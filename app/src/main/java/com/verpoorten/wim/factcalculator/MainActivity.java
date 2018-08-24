d96cc5350aa6f71fd8b8631be73bd3724a7c05da
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-08-24 12:05:25 +0200

    MainActivity ok, test toast ok, setup gsm for testing ok

--- app/src/main/java/com/verpoorten/wim/factcalculator/MainActivity.java
+++ app/src/main/java/com/verpoorten/wim/factcalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.verpoorten.wim.factcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("FactCalculator");
+
+        Toast.makeText(MainActivity.this, "FactCalculator initaiting, oh Master...", Toast.LENGTH_LONG).show();
     }
 }
