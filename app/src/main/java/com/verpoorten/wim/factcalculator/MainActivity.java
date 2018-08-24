574527a0f4a06dc108aa4d0a54d6acac1204a9ec
Wim Verpoorten <wim.verpoorten@hotmail.com>
2018-08-24 14:57:20 +0200

    0 button klik wordt opgevangen

--- app/src/main/java/com/verpoorten/wim/factcalculator/MainActivity.java
+++ app/src/main/java/com/verpoorten/wim/factcalculator/MainActivity.java
@@ -2,9 +2,13 @@ package com.verpoorten.wim.factcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button input_0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setTitle("FactCalculator");
 
         Toast.makeText(MainActivity.this, "FactCalculator initaiting, oh Master...", Toast.LENGTH_LONG).show();
+
+
+        input_0 = (Button) findViewById(R.id.button_0);
+        input_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "you pressed the zero nothing nada key, dude!", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
